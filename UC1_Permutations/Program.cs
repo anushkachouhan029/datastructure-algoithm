c003c4104a5b105c468fadd09a0f36c67993444c
anushkachouhan029 <anushkachouhan029>
2023-01-27 09:49:51 +0530

    added binary

--- UC1_Permutations/Program.cs
+++ UC1_Permutations/Program.cs
@@ -2,30 +2,46 @@
 {
     class Program
     {
-        static void Main(String[] args)
+        static void Main(string[] args)
         {
-            string flag = "Y";
-            while (flag == "Y" || flag == "y")
+            Console.WriteLine("Welcome to the Algorithm Programs\n=================================");
+            bool end = true;
+            Console.WriteLine("1. Permutation of a word\n2. Binary Search\n3. End of program");
+            while (end)
             {
-                Console.WriteLine("Welcome To Datastructures and Algorithms");
-                Console.WriteLine("1.Permutation Using Recursion");
-                Console.WriteLine("Enter Your Option");
-                int num = Convert.ToInt32(Console.ReadLine());
-                switch (num)
+                Console.Write("Choose an option to execute : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
                 {
                     case 1:
-                        PermutationsOfString permutationsofstring = new PermutationsOfString();
-                        permutationsofstring.permutations();
+                        PermutationsOfString permutation = new PermutationsOfString();
+                        permutation.permutations();
                         break;
-
+                    case 2:
+                        const string FILE_PATH = @"D:\RFP BATCH 157\AlgorithmProgram\Day16_and_17_AlgorithmPrograms\BinarySearchList.txt";
+                        BInarySearchProb search = new BInarySearchProb();
+                        string input_String = File.ReadAllText(FILE_PATH);
+                        string[] array = input_String.Split(',');
+                        int length = array.Length;
+                        Console.Write("Enter the word to Search : ");
+                        string key = Console.ReadLine();
+                        int result = search.BinarySearchElement(array, 0, length - 1, key);
+                        if (result == -1)
+                        {
+                            Console.WriteLine(key + " word is present in the file");
+                        }
+                        else
+                        {
+                            Console.WriteLine(key + " Word is  not present ");
+                        }
+                        break;
+                    case 3:
+                        end = false;
+                        break;
                     default:
-                        Console.WriteLine("-------Enter the valid option---");
+                        Console.WriteLine("You choose an invalid option ");
                         break;
                 }
-                Console.WriteLine("\nDo you want to continue");
-                flag = Console.ReadLine();
             }
-            Console.ReadKey();
         }
     }
-}
